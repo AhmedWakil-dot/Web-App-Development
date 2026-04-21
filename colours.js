cdd41910a26cf01e105280d964f01ceca816ee82
AhmedWakil-dot <a.m.wakil@edu.salford.ac.uk>
2026-04-21 15:23:02 +0100

    Add Colors object with color definitions

--- colours.js
+++ colours.js
@@ -1,1 +1,9 @@
 
+export const Colors = {
+  primaryBlue: '#4262ff',    // --blue700
+  darkIndigo: '#050038',     // --indigo700
+  lightBackground: '#f0f0f3', // --indigo100
+  errorRed: '#ff4d4d',
+  white: '#ffffff',
+  textSecondary: '#9b99af',  // --indigo500
+};
